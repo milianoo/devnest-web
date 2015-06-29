0b9ae8e7fbc9439390f9a20a8e8b83ead1ed0e23
milianoo <milad.rk@live.com>
2015-06-29 15:04:44 +0000

    let's do this

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var bodyParser = require('body-parser');
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: true })
+
+app.use(express.static(path.resolve(__dirname, 'client')));
+
+app.get('/home', function (req, res) {
+  var email = req.query.email; 
+  console.log(email);
+   res.sendFile( __dirname + "/client/" + "index.html" );
+})
+
+app.post('/', function (req, res) {
+
+   // Prepare output in JSON format
+   var response = {
+       first_name:req.body.email
+   };
+   console.log(response);
+   res.sendFile( __dirname + "/client/" + "index.html" );
+})
+
+app.post('/registeremail',urlencodedParser, function (req, res) {
+
+   var response = {
+       email:req.body.email
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+var server = app.listen(app.get('port'), function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("app started listening at http://%s:%s", host, port)
+
+})
